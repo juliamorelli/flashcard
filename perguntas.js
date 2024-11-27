7054a40849d93b2155d7e95961174b526d145a79
juliamorelli <morellix7@gmail.com>
2024-11-27 10:53:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'Qual maior osso do corpo humano?',
+    'Fêmur'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o solo de Jaú?',
+    'Latosolo, mais conhecido como terra roxa'
+)
+
+criaCartao(
+    'História',
+    'Em que ano ocorreu a ebolição da escravatura no Brasil?',
+    '1888'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz maça em Inglês?',
+    'Aplle'
+)
